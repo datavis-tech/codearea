ee095c3189a4b3cf11a418308b6d69d7f7094eb0
Curran <curran.kelleher@gmail.com>
2019-08-03 15:22:40 +0530

    Render output from walked tree

--- test-app/src/CodeAreaHighlighter.js
+++ test-app/src/CodeAreaHighlighter.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Parser from 'web-tree-sitter';
 
 const setupParser = async () => {
@@ -9,18 +9,21 @@ const setupParser = async () => {
   return parser;
 };
 
-let tokens;
-const walk = node => {
+const walk = (node, tokens) => {
   if (node.childCount > 0 && node.type !== 'string') {
-    node.children.forEach(walk);
+    for (let i = 0; i < node.childCount; i++) {
+      walk(node.children[i], tokens);
+    }
   } else {
     tokens.push(node.text);
   }
+  return tokens;
 };
 
 export const CodeAreaHighlighter = ({ text$ }) => {
   const [text, setText] = useState();
   const [parser, setParser] = useState();
+  const [tokens, setTokens] = useState();
 
   useEffect(() => text$.subscribe(setText).unsubscribe, [text$, setText]);
 
@@ -35,10 +38,11 @@ export const CodeAreaHighlighter = ({ text$ }) => {
     const tree = parser.parse(text);
     console.log(tree.rootNode.toString());
 
-    tokens = [];
-    walk(tree.rootNode);
-    console.log(tokens);
-  }, [parser]);
+    setTokens(walk(tree.rootNode, []));
+  }, [parser, text]);
 
-  return <pre>{text}</pre>;
+  return (
+    <pre>{tokens ? tokens.map((token, i) => <span key={i}>{token}</span>) : text}</pre>
+  );
+  //return <pre>{text}</pre>;
 };
