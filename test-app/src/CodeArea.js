5b6ae41743e87db3ab3fbc731d6cb0c0496e17ce
Curran <curran.kelleher@gmail.com>
2019-08-03 15:00:53 +0530

    Walk the tree

--- test-app/src/CodeArea.js
+++ test-app/src/CodeArea.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { CodeAreaTextArea } from './CodeAreaTextArea';
 import { CodeAreaHighlighter } from './CodeAreaHighlighter';
-import "./CodeArea.css";
+import './CodeArea.css';
 
 export const CodeArea = ({ text$ }) => {
   return (
-    <div className="code-area" >
+    <div className="code-area">
       <CodeAreaTextArea text$={text$} />
       <CodeAreaHighlighter text$={text$} />
     </div>
--- test-app/src/CodeAreaHighlighter.js
+++ test-app/src/CodeAreaHighlighter.js
@@ -4,23 +4,41 @@ import Parser from 'web-tree-sitter';
 const setupParser = async () => {
   await Parser.init();
   const JavaScript = await Parser.Language.load('tree-sitter-javascript.wasm');
-  const parser = new Parser;
+  const parser = new Parser();
   parser.setLanguage(JavaScript);
-  const sourceCode = 'let x = 1; console.log(x);';
-  const tree = parser.parse(sourceCode);
-  console.log(tree.rootNode.toString());
+  return parser;
+};
+
+let tokens;
+const walk = node => {
+  if (node.childCount > 0 && node.type !== 'string') {
+    node.children.forEach(walk);
+  } else {
+    tokens.push(node.text);
+  }
 };
 
 export const CodeAreaHighlighter = ({ text$ }) => {
   const [text, setText] = useState();
+  const [parser, setParser] = useState();
 
   useEffect(() => text$.subscribe(setText).unsubscribe, [text$, setText]);
 
   useEffect(() => {
-    setupParser();
+    setupParser().then(setParser);
   }, []);
 
-  return (
-    <pre>{text}</pre>
-  );
+  useEffect(() => {
+    if (!parser) {
+      return;
+    }
+    const tree = parser.parse(text);
+    console.log(tree.rootNode.toString());
+
+    tokens = [];
+    walk(tree.rootNode);
+    console.log(tokens);
+  }, [parser]);
+
+  return <pre>{text}</pre>;
 };
