3df2a05f1e85c7e592e72a439695638eba27293c
Curran <curran.kelleher@gmail.com>
2019-08-03 18:14:30 +0530

    Add README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# codearea
-Experimental code editor with syntax highlighting.
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+To start:
+
+`npm start` to serve the React app.
+
+`node server.js` to serve the Node.js server (required for WASM issues).
---
+++ build/precache-manifest.a29e75eeb7121bd6b5db4a9229aa031f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121a39e9f653aeeb1ff33a6907ec48c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "478127d422c3433b08db",
+    "url": "/static/css/main.9c9ac02f.chunk.css"
+  },
+  {
+    "revision": "12f95fcdfaa020f5509b",
+    "url": "/static/js/2.2a17d27b.chunk.js"
+  },
+  {
+    "revision": "478127d422c3433b08db",
+    "url": "/static/js/main.7cf5a54a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
